27c144e93d638c4b14f8ebbd95604435491bbc58
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-15 14:40:36 +0530

    Add Navbar component with brand logo and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,24 @@
+import BrandLogo from "./BrandLogo";
+import { BRAND } from "../config/brand";
 
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__brand">
+        <BrandLogo className="navbar__logo" />
+
+        <span className="navbar__name">
+          {BRAND.name}
+        </span>
+      </div>
+
+      <nav className="navbar__links">
+        <a href="/">Menu</a>
+        <a href="/orders">Orders</a>
+        <a href="/profile">Profile</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
