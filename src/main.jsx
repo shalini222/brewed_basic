66a600134577c842528cd92d7dc827be6c53369f
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-15 14:20:05 +0530

    Refactor main.jsx to include theme application
    Added applyTheme function call before rendering the app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { applyTheme } from "./config/theme";
 
-createRoot(document.getElementById('root')).render(
+applyTheme();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
